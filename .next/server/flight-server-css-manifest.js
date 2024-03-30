8584ff0f5084b8c34bc5abb9694006b2a181af39
Darshil Dhandhiya <darshil.dhandhiya03@gmail.com>
2024-03-31 02:12:37 +0530

    Update

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,25 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/app/page.tsx": [
+      "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/app/home.module.css",
+      "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/node_modules/next/font/google/target.css?{\"path\":\"constant/font.ts\",\"import\":\"Montserrat\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":[\"100\",\"200\",\"300\",\"400\",\"500\",\"600\",\"700\",\"800\",\"900\"],\"variable\":\"--font-montserrat\",\"display\":\"swap\"}],\"variableName\":\"montserrat\"}",
+      "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/node_modules/next/font/google/target.css?{\"path\":\"constant/font.ts\",\"import\":\"Poppins\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":[\"100\",\"200\",\"300\",\"400\",\"500\",\"600\",\"700\",\"800\",\"900\"],\"variable\":\"--font-poppins\",\"display\":\"swap\"}],\"variableName\":\"poppins\"}",
+      "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/node_modules/next/font/local/target.css?{\"path\":\"constant/font.ts\",\"import\":\"\",\"arguments\":[{\"src\":\"../assets/fonts/suarte/Suarte Free.ttf\",\"display\":\"swap\"}],\"variableName\":\"suarte\"}"
+    ],
+    "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/app/layout.tsx": [
+      "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/app/globals.css",
+      "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/node_modules/next/font/local/target.css?{\"path\":\"constant/font.ts\",\"import\":\"\",\"arguments\":[{\"src\":\"../assets/fonts/suarte/Suarte Free.ttf\",\"display\":\"swap\"}],\"variableName\":\"suarte\"}",
+      "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/node_modules/next/font/google/target.css?{\"path\":\"constant/font.ts\",\"import\":\"Montserrat\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":[\"100\",\"200\",\"300\",\"400\",\"500\",\"600\",\"700\",\"800\",\"900\"],\"variable\":\"--font-montserrat\",\"display\":\"swap\"}],\"variableName\":\"montserrat\"}",
+      "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/node_modules/next/font/google/target.css?{\"path\":\"constant/font.ts\",\"import\":\"Poppins\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":[\"100\",\"200\",\"300\",\"400\",\"500\",\"600\",\"700\",\"800\",\"900\"],\"variable\":\"--font-poppins\",\"display\":\"swap\"}],\"variableName\":\"poppins\"}"
+    ]
+  },
+  "cssModules": {
+    "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/app/page": [
+      "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/node_modules/next/font/google/target.css?{\"path\":\"constant/font.ts\",\"import\":\"Montserrat\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":[\"100\",\"200\",\"300\",\"400\",\"500\",\"600\",\"700\",\"800\",\"900\"],\"variable\":\"--font-montserrat\",\"display\":\"swap\"}],\"variableName\":\"montserrat\"}",
+      "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/node_modules/next/font/google/target.css?{\"path\":\"constant/font.ts\",\"import\":\"Poppins\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":[\"100\",\"200\",\"300\",\"400\",\"500\",\"600\",\"700\",\"800\",\"900\"],\"variable\":\"--font-poppins\",\"display\":\"swap\"}],\"variableName\":\"poppins\"}",
+      "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/node_modules/next/font/local/target.css?{\"path\":\"constant/font.ts\",\"import\":\"\",\"arguments\":[{\"src\":\"../assets/fonts/suarte/Suarte Free.ttf\",\"display\":\"swap\"}],\"variableName\":\"suarte\"}",
+      "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/app/globals.css",
+      "/Users/darshildhandhiya/Documents/Study/Projects/Portfolio/app/home.module.css"
+    ]
+  }
+}
