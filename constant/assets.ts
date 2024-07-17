d7c8978711610dd44a169ccaf42ffc9af3dabe9a
Darshil Dhandhiya <darshil.dhandhiya03@gmail.com>
2024-07-17 22:23:06 +0530

    Update

--- constant/assets.ts
+++ constant/assets.ts
@@ -2,6 +2,7 @@ export const assets = {
   home: {
     hero: {
       avatarSmile: require("@images/home/hero/avatar-smile.webp"),
+      dribble: require("@images/home/hero/dribble.webp"),
       github: require("@images/home/hero/github.webp"),
       instagram: require("@images/home/hero/instagram.webp"),
       linkedin: require("@images/home/hero/linkedin.webp"),
